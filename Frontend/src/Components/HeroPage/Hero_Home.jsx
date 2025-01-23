1a0b4f066f50ebb7babd7d497610114f216e8953
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-23 15:18:48 +0430

    New changes in hero

--- Frontend/src/Components/HeroPage/Hero_Home.jsx
+++ Frontend/src/Components/HeroPage/Hero_Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero_Home = () => {
   return (
     <div>
-      
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit placeat est deleniti, sapiente commodi aspernatur vel voluptatem! Aut dolores rerum perferendis doloremque amet autem incidunt dicta at debitis nam. Harum!
     </div>
   )
 }
