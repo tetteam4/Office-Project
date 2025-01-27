d713deec21f9fbc9a37e020c11273f626ac1e0b2
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-27 17:46:23 +0430

    make the blogCard

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 import Hero_Home from '../Components/HeroPage/Hero_Home';
 import Tet_Detials from '../Components/Home/Tet_Detials';
 import BeCreative from '../Components/Home/BeCreative';
+import BlogCard from '../Components/Blog/BlogCard';
 
 
 
 const HomePage = () => {
  
   return (
-    <div className="  font-sans ">
+    <div className=" container mx-auto  font-sans ">
     <Hero_Home /> 
     {/* TET */}
     <Tet_Detials />
     {/* Be Creative section */}
     <BeCreative />
+    {/* <BlogCard /> */}
       {/* Hero Section */} 
       <section className="bg-blue-700 text-white py-20">
         <div className="max-w-7xl mx-auto px-6 text-center">
