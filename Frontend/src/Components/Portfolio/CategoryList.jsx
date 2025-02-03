4bd521384bd045d764324592922cc24fd0e514eb
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-02-03 19:46:34 +0430

    work in portfolio section

---
+++ Frontend/src/Components/Portfolio/CategoryList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { MdCategory } from "react-icons/md";
+
+const CategoryList = ({ Portfolio_Data }) => {
+  return (
+    <div className="h-full">
+      <ul className="">
+        {Portfolio_Data.map((port, index) => (
+          <li className="flex items-center border-b cursor-pointer border-gray-300 hover:bg-white p-3 gap-x-2">
+            <span className="">
+              <MdCategory size={40} />
+            </span>
+            <p className="">
+              <span className="text-gray-700 text-sm">web design portfolio </span>
+              <span className="font-bold text-md"> {port.category}</span>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
