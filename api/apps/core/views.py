ba1e394ac2e1395b0583c24f1e30be7746bd075c
alisinasultani255@gmail.com <alisinasultani255@gmail.com>
2025-02-05 23:34:31 +0430

    update and delete category

--- api/apps/core/views.py
+++ api/apps/core/views.py
@@ -1,38 +1,18 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions, viewsets
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
-from rest_framework.response import Response
+from rest_framework import generics, permissions
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
 
-from .models import BlogPost, Category
-from .serializers import BlogPostSerializer, CategorySerializer
-
-
-class BlogPostListView(generics.GenericAPIView, ListModelMixin):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer
-    permission_classes = [permissions.AllowAny]
-
-    def get_queryset(self):
-        queryset = BlogPost.objects.all()
-        category = self.request.query_params.get("category", None)
-        if category:
-            queryset = queryset.filter(category__name__icontains=category)
-        return queryset
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+from .models import Category
+from .serializers import CategorySerializer
 
 
-class BlogPostDetailView(generics.GenericAPIView, RetrieveModelMixin):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer
-    permission_classes = [permissions.AllowAny]
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class CategoryListView(generics.GenericAPIView, ListModelMixin):
+# View for listing and creating categories
+class CategoryListView(generics.GenericAPIView, ListModelMixin, CreateModelMixin):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.AllowAny]
@@ -40,11 +20,26 @@ class CategoryListView(generics.GenericAPIView, ListModelMixin):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class CategoryDetailView(generics.GenericAPIView, RetrieveModelMixin):
+
+class CategoryDetailView(
+    generics.GenericAPIView,
+    RetrieveModelMixin,  # Add this mixin
+    UpdateModelMixin,
+    DestroyModelMixin,
+):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.AllowAny]
+    lookup_field = "id"  # Use UUID as the lookup field
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
